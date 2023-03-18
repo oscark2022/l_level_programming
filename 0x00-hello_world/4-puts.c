c664c2dc1cb77a166f6f02757608b7ff25058579
oscark <oscark535@gmail.com>
2023-03-18 13:05:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * retur:0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
