780940ad90458b6d80e4e79a5b43c7113e8f4648
oscark <oscark535@gmail.com>
2023-03-19 02:35:05 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry point into a code that prints size of dft comps
+ *Return: 0(Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("Size of a char: %lu byte(s)\n,(unsigned long)size of (a)");
+printf("Size of an int: %lu byte(s)\n,(unsigned long)size of (b)");
+printf("Size of a long int: %lu byte(s)\n,(unsigned long)size of (c)");
+printf("Size of a long long int: %lu byte(s)\n,(unsigned long)size of (d)");
+printf("Size of a float: %lu byte(s)\n,(unsigned long)size of (d)");
+return (0);
+}
