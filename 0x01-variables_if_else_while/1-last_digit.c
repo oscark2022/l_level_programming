de9e9e739185d98e5a8bd5a57e25216f604ef6dc
oscark <oscark535@gmail.com>
2023-03-20 17:50:35 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,12 +5,14 @@
 *main - Entry point into a code that prints size of dft com
 *Return: 0(Success)
 */
+
 int main(void)
 {
 int n, lastd;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastd = n % 10;
+
 if (lastd  > 5)
 {
 printf("last digit of %d is %d and is greater than 5\n", n, lastd);
