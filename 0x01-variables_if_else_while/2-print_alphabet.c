7d1f67d024ee3c288fabb081c8112b815e1bd664
oscark <oscark535@gmail.com>
2023-03-25 21:57:44 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,16 @@
-#include "main.h"
+#include <stdio.h>
 /**
  *main - Entry point into a code that prints size of dft comps
  *Return: 0(Success)
  */
 int main(void)
 {
-print_alphabet();
+char alp[26] - "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[1]);
+}
+putchar('\n');
 return (0);
 }
