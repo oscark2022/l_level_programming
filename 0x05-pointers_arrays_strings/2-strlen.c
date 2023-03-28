67ce9116567837efd749ed21fa87445db054fc65
oscark <oscark535@gmail.com>
2023-03-28 07:51:11 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - length of a string
+ * return (length)
+ */
+int _strlen(char *S)
+{
+	int longi = 0;
+
+	while (*S != '\0')
+{
+longi++;
+S++;
+}
+return (Longi);
+}
