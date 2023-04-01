b3bf8b38efbe9f326d67c17a627431845e429b1b
oscark <oscark535@gmail.com>
2023-04-01 08:36:52 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,36 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
-*main - Entry point into a code that prints size of dft com
-*Return: 0(Success)
-*/
 
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int n, lastd;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-lastd = n % 10;
+	int n;
 
-if (lastd  > 5)
-{
-printf("last digit of %d is %d and is greater than 5\n", n, lastd);
-}
-else if (lastd == 0)
-{
-printf("last digit of %d is %d and is zero\n", n, lastd);
-}
-else if (lastd < 6 && lastd != 0)
-{
-printf("last digit of %d is %d and is less than 6 and not zero\n", n, lastd);
-}
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",
+			n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			n, n % 10);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n",
+			n, n % 10);
+	}
+
+	return (0);
+
 }
