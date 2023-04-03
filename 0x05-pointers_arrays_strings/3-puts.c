d8bc8352d191adbe1a84825758afadcde1dca894
oscark <oscark535@gmail.com>
2023-04-03 10:02:54 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,15 +2,17 @@
 /**
  * _puts - prints a string
  * @str: the string
+ *
  * Return: the length of a string
  */
 void _puts(char *str)
 {
-int i = 0;
-while (str[i])
-{
-_putchar(str[i]);
-i++;
-}
-_putchar('\n');
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
